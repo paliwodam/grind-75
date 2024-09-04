a6576fc3bd392dd892eb3c5d6b6652c9357e4c30
Martyna Paliwoda <martynapaliwoda.2001@gmail.com>
2024-09-04 23:31:26 +0200

    [Accounts Merge] Union-Find solution from leetcode community

--- week4/accounts-merge.py
+++ week4/accounts-merge.py
@@ -2,38 +2,33 @@
 # Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
 # After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
 
-from typing import List
 from collections import defaultdict
+from typing import List
+
+class UF:
+    def __init__(self, N):
+        self.parents = list(range(N))
+
+    def union(self, child, parent):
+        self.parents[self.find(child)] = self.find(parent)
+
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
 
 class Solution:
-
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        def DFS(mail, edges, visited, mail_group):
-            visited[mail] = True
-            mail_group.append(mail)
+        uf = UF(len(accounts))
+        
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
+            for email in emails:
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
+        ans = defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
 
-            for neigh in edges[mail]:
-                if not visited[neigh]:
-                    DFS(neigh, edges, visited, mail_group)
-            return mail_group
-
-        accounts_dict = defaultdict(list)
-        for account in accounts:
-            name, *emails = account
-            accounts_dict[name].append(emails)
-
-        result = []
-        for name, accounts in accounts_dict.items():
-            edges = defaultdict(list)
-            for mails in accounts:
-                for mail in mails:
-                    edges[mail].extend(other_mail for other_mail in mails if mail != other_mail)
-            
-            visited = {mail: False for mail in edges.keys()}
-
-            for mail in edges.keys():
-                if not visited[mail]:
-                    mail_group = sorted(DFS(mail, edges, visited, []))
-                    result.append([name] + mail_group)
-
-        return result
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
